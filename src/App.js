c443b6a89282e3338b2454d03b4548f8af376cfc
Clive07 <86563613+Clive07@users.noreply.github.com>
2021-09-15 20:23:17 +0100

    initial commit (#1)
    Set up header with none functioning navbar.
    
    prepared css file for actual design.
    
    Added more to ReadMe Approach
    
    Set up Landing Page.
    
    Ready to begin API Call for starter list from land page.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import HeaderContainer from "./Components/Header/HeaderContainerComponent";
 import MainContainer from "./Components/Main/MainContainerComponent";
-import "./styles.css";
+import "./styles/styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>And So Our Story Begins!</h2>
       <HeaderContainer />
       <MainContainer />
     </div>
